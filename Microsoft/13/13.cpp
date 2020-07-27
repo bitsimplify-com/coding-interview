42d96ef6f24b12db05cf51ad201efaa354de804a
Arun Arumugham <38404939+arunvedha@users.noreply.github.com>
2020-07-27 14:26:09 +0530

    Add files via upload

---
+++ Microsoft/13/13.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+class sample
+{
+public:
+    bool isMatch(string A, string B)
+    {
+        int i = 0, j = 0, m = A.size(), n = B.size();
+        bool dp[m + 1][n + 1];
+        for (int i = 0; i <= m; i++)
+        {
+            for (int j = 0; j <= n; j++)
+            {
+                if (i == 0 && j == 0)
+                    dp[i][j] = true;
+                else if (i == 0 && B[j - 1] == '*')
+                    dp[i][j] = dp[i][j - 1];
+                else if (i == 0)
+                    dp[i][j] = false;
+                else if (j == 0)
+                    dp[i][j] = false;
+                else
+                {
+                    if (A[i - 1] == B[j - 1] || B[j - 1] == '?')
+                        dp[i][j] = dp[i - 1][j - 1];
+                    else if (B[j - 1] == '*')
+                        dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                    else
+                        dp[i][j] = false;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
+
+int main()
+{
+    sample g;
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+    cout << g.maxProduct(nums);
+    return 0;
+}
