0d4735ebf4293cbca6bb8d3ede380fd16c8822f2
Arun Arumugham <38404939+arunvedha@users.noreply.github.com>
2020-07-27 21:16:54 +0530

    Create 2.cpp

---
+++ backtracking/2/2.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+class sample {
+public:
+    vector<vector<int>> result;
+    vector<int> current;
+    void dfs(vector<int>& nums,int start){
+        if(current.size()==nums.size()){
+            result.push_back(current);
+            return;
+        }
+        unordered_set<int> uset;
+        for(int i=start;i<nums.size();i++){
+            if(uset.find(nums[i])!=uset.end())continue;
+            current.push_back(nums[i]);
+            uset.insert(nums[i]);
+            swap(nums[i],nums[start]);
+            dfs(nums,start+1);
+            swap(nums[i],nums[start]);
+            current.pop_back();
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        dfs(nums,0);
+        return result;
+    }
+};
+
+int main()
+{
+    sample g;
+    int n;
+    cin>>n;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    vector<vector<int>> soln;
+    soln= g.permute(nums);
+    for(int i=0;i<soln.size();i++){
+        for(int j=0;j<n;j++){
+            cout<<soln[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
